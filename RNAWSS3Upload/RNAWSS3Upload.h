ccf0819149bb68beb684d9700fd90b8c4fe4ebe3
watzak <kzahirag@gmail.com>
2015-11-12 19:23:07 +0100

    Initial Commit

---
+++ RNAWSS3Upload/RNAWSS3Upload.h
@@ -0,0 +1,13 @@
+//
+//  RNAWSS3Upload.h
+//  RNAWSS3Upload
+//
+//  Created by watzak on 12/11/15.
+//  Copyright © 2015 watzak. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RNAWSS3Upload : NSObject
+
+@end
---
+++ RNAWSS3Upload/RNAWSS3Upload.m
@@ -0,0 +1,13 @@
+//
+//  RNAWSS3Upload.m
+//  RNAWSS3Upload
+//
+//  Created by watzak on 12/11/15.
+//  Copyright © 2015 watzak. All rights reserved.
+//
+
+#import "RNAWSS3Upload.h"
+
+@implementation RNAWSS3Upload
+
+@end
